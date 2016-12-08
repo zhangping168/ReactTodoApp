bba69840ec926aad20f4ef9ca954ba728f83dd2b
zhangping168@gmail.com <zhangping168@gmail.com>
2016-12-08 11:25:51 -0500

    Add todo app

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+  render: function(){
+    return (
+      <div>
+        <h1>Todo App</h1>
+      </div>
+    );
+  }
+
+});
+
+module.exports = TodoApp;
